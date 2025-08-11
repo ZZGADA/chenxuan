98134b02d47c51156204572a7aab811d7cae3422
zhangzan <zhangzan@tal.com>
2025-08-11 21:10:29 +0800

    fresh

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 50308;
 const HOST = process.env.HOST || '0.0.0.0';
 
 // 提供静态文件服务
